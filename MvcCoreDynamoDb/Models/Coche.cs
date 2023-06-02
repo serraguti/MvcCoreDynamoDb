bac3f00a116f31498b539f188c1c0d5a340f82fd
serraguti <pacoserranox@gmail.com>
2023-06-02 10:22:20 +0200

    Add project files.

---
+++ MvcCoreDynamoDb/Models/Coche.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace MvcCoreDynamoDb.Models
+{
+    [DynamoDBTable("coches")]
+    public class Coche
+    {
+        [DynamoDBHashKey]
+        [DynamoDBProperty("idcoche")]
+        public int IdCoche { get; set; }
+        [DynamoDBRangeKey]
+        [DynamoDBProperty("marca")]
+        public string Marca { get; set; }
+        [DynamoDBProperty("modelo")]
+        public string Modelo { get; set; }
+        [DynamoDBProperty("imagen")]
+        public string Imagen { get; set; }
+    }
+}
